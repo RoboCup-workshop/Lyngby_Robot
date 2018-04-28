e92b4c4c2e37cacebffa0891a88af620edbfffc2
JoachimSand <37040245+JoachimSand@users.noreply.github.com>
2018-04-28 17:11:40 +0200

    Add files via upload

--- DetectBil.ino
+++ DetectBil.ino
@@ -1,6 +1,5 @@
 #include <Pixy.h>
 #include <SPI.h>  
-
 Pixy pixy;
 uint16_t AverageArea; 
 uint16_t CountAverage; 
@@ -8,13 +7,11 @@ uint16_t CountThisRound;
 uint32_t TotalArea;
 uint16_t AverageThisRound;
 uint16_t TotalThisRound;
-
 void setup() {
   // put your setup code here, to run once:
  Serial.begin(9600);
  pixy.init();   
 }
-
 void loop() {
   uint16_t blocks;
   char buf[32];
@@ -26,37 +23,26 @@ void loop() {
     CountAverage += 1;
     TotalArea = TotalArea + pixy.blocks[j].width * pixy.blocks[j].height;
     AverageArea = TotalArea/CountAverage;
-
     CountThisRound += 1;
     TotalThisRound = TotalThisRound + pixy.blocks[j].width * pixy.blocks[j].height;
     AverageThisRound = TotalThisRound/CountThisRound;
-        
     }
     if (AverageThisRound < AverageArea * 0.5 && AverageThisRound > 0){
       Serial.println("===========================Der er en bil.===============================");
   }
-    if (AverageThisRound > 0){
-      
-    }
     if (AverageArea < 70){
       AverageArea = 71;
   }
   k++;
   if(k%100==0){
-  
   Serial.print(" ATR ");
   Serial.print(AverageThisRound);
   Serial.print(" AA ");
   Serial.print(AverageArea);  
   Serial.print(" K ");
   Serial.println(k);
-
-  
-  
   AverageThisRound = 0;
   TotalThisRound = 0;
   CountThisRound = 0;
-   
   }
-
 }
